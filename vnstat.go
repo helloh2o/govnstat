671f2ddc0dab08b65f40653c3a27bc0422a3523c
helloh2o <54695727+helloh2o@users.noreply.github.com>
2021-07-07 18:25:32 +0800

    Update vnstat.go

--- vnstat.go
+++ vnstat.go
@@ -1,11 +1,9 @@
 package main
 
 import (
+	"cowxy2/cmm"
 	"flag"
-	"fmt"
-	"log"
-	"io/ioutil"
-	"os/exec"
+	"github.com/helloh2o/lucky/log"
 	"strconv"
 	"strings"
 	"time"
@@ -37,104 +35,45 @@ func main() {
 
 func check() {
 	if *ver == 1 {
-		RunCommand("vnstat", "-u")
+		cmm.RunCommand("vnstat", "-u")
 	}
-	result := RunCommandWith("vnstat", *args)
+	result := cmm.RunCommandWith("vnstat", *args)
 	lines := strings.Split(result, "\n")
 	switch *args {
 	case "-m":
-		// date
-		fmtStr := "%d-%d"
-		y := time.Now().Year()
-		m := time.Now().Month()
-		ys := strconv.Itoa(y) + "-"
-		month := ""
+		dataLines := make([]string, 0)
 		for _, line := range lines {
-			vvs := strings.Split(line, "|")
-			for i, v := range vvs {
-				if i == 0 {
-					// not month data break
-					if !strings.Contains(v, ys) {
-						break
-					}
-					if m < 10 {
-						fmtStr = "%d-0%d"
-					}
-					month = fmt.Sprintf(fmtStr, y, m)
-					if !strings.Contains(v, month) {
-						log.Printf("Skip passed month data %s", v)
-						break
-					}
-				}
-				if i == 2 {
-					v = strings.Trim(v, " ")
-					total := strings.Split(v, " ")
-					if len(total) == 2 {
-						amount, _ := strconv.ParseFloat(total[0], 32)
-						dw := total[1]
-						switch dw {
-						case GiB:
-							if amount < *max {
-								log.Printf("%s traffic ok, used:: %.2f, left:: %.2f", month, amount, *max-amount)
-							} else {
-								log.Printf("%s -_-, oops traffic used up, god!!! exec poweroff cmd now.", month)
-								RunCommandWith("poweroff")
-							}
-						default:
-							log.Printf("ignore the wd -> %s", dw)
-							return
+			if strings.Contains(line, "bit/s") {
+				dataLines = append(dataLines, line)
+			}
+		}
+		// recent month
+		recent := dataLines[len(dataLines)-1]
+		vvs := strings.Split(recent, "|")
+		for i, v := range vvs {
+			if i == 2 {
+				v = strings.Trim(v, " ")
+				total := strings.Split(v, " ")
+				if len(total) == 2 {
+					amount, _ := strconv.ParseFloat(total[0], 32)
+					dw := total[1]
+					switch dw {
+					case GiB:
+						if amount < *max {
+							log.Release("traffic ok, used:: %.2f, left:: %.2f \norigin data::%s", amount, *max-amount, recent)
+						} else {
+							log.Error("oops traffic used up, god!!! exec poweroff cmd now.")
+							cmm.RunCommandWith("poweroff")
 						}
+					default:
+						log.Release("ignore the wd -> %s", dw)
+						return
 					}
 				}
 			}
 		}
 	case "--json":
 	default:
-		log.Printf("unhandle result:: %s", result)
+		log.Release("unhandle result:: %s", result)
 	}
-}
-
-
-func RunCommand(cmdName string, arg ...string) {
-	cmd := exec.Command(cmdName, arg...)
-	stdout, err := cmd.StdoutPipe()
-	defer stdout.Close()
-	if err != nil {
-		panic(err)
-		return
-	}
-	// run
-	if err := cmd.Start(); err != nil {
-		panic(err)
-		return
-	}
-	// result
-	opBytes, err := ioutil.ReadAll(stdout)
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	log.Println(string(opBytes))
-}
-
-func RunCommandWith(cmdName string, arg ...string) string {
-	cmd := exec.Command(cmdName, arg...)
-	stdout, err := cmd.StdoutPipe()
-	defer stdout.Close()
-	if err != nil {
-		panic(err)
-		return err.Error()
-	}
-	// run
-	if err := cmd.Start(); err != nil {
-		panic(err)
-		return err.Error()
-	}
-	// result
-	opBytes, err := ioutil.ReadAll(stdout)
-	if err != nil {
-		log.Println(err.Error())
-		return err.Error()
-	}
-	return string(opBytes)
 }
